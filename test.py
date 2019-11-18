ede927bfb0680ee1799477777d70550252cf2b8d
benjaminlewis-1000 <benjamin.lewis.13@us.af.mil>
2019-11-18 13:49:08 -0500

    More work

--- test.py
+++ test.py
@@ -5,53 +5,50 @@ import face_recognition
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import imagehash
+from PIL import Image
+import time
+
 
 file = '/home/lewisbp/snippets/face_id/test_imgs/101_2979.JPG'
 
-npImage = face_recognition.load_image_file(file)
+data = cv2.imread(file)
 
-h, w, c = npImage.shape
-resize_ratio = 5
-num_upsamples = 2
+s = time.time()
+height, width, chan = data.shape
 
-resized_chip = cv2.resize(npImage, \
-    ( int( w / resize_ratio ), \
-      int( h / resize_ratio ) ) )
+sq_size = np.min(( height, width) ) // 2
 
-# top, right, bottom, left
-face_locations = face_recognition.face_locations(resized_chip, \
-                    number_of_times_to_upsample=num_upsamples,  model='cnn')
+left = width // 2 - sq_size
+right = width // 2 + sq_size
+top = height // 2 - sq_size
+bot = height // 2 + sq_size
 
-for t in face_locations:
-    top, right, bottom, left = t
+square_img = data[top:bot, left:right]
+# print(square_img.shape)
+print(time.time() - s)
 
-cv2.rectangle(resized_chip, (left, top), (right, bottom), (255, 50, 50), 4)
-cv2.rectangle(npImage, (left * resize_ratio, top * resize_ratio), (right * resize_ratio, bottom * resize_ratio), (255, 50, 50), 4)
+small_px = 16
 
-f_prime = [(top * resize_ratio, right * resize_ratio, bottom * resize_ratio, left * resize_ratio)]
+small = cv2.resize(square_img, (small_px, small_px) )
+small_gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
 
-n_jit = 100
-identity = face_recognition.face_encodings(resized_chip, known_face_locations=face_locations, num_jitters=n_jit)
-identity_prime = face_recognition.face_encodings(npImage, known_face_locations=f_prime, num_jitters=n_jit)
+mean = np.mean(small_gray)
+small_bool = np.where(small_gray > mean, 1, 0)
 
-for i in range(-1, 5):
-    f_prime_move  = [(top * resize_ratio + i, right * resize_ratio + i, bottom * resize_ratio + i, left * resize_ratio + i)]
-    identity_prime_move = face_recognition.face_encodings(npImage, known_face_locations=f_prime_move, num_jitters=n_jit)
-    print(np.linalg.norm(identity_prime_move[0] - identity_prime[0]))
+one_d = small_bool.reshape(-1)
 
-plt.imshow(resized_chip)
-plt.show(block=False)
-plt.figure()
-plt.imshow(npImage)
-plt.show()
+byte_array = np.packbits(one_d)
 
+hash_val = ''.join('{:02x}'.format(x) for x in byte_array)
 
+print(hash_val, time.time() - s)
 
-# chip_part = cv_image[top_edge:bottom_edge, left_edge:right_edge]
+import xxhash
 
-# Then resize it to fit in the GPU memory, based 
-# on the parameters passed to the function.
-# height_chip = chip_part.shape[0]
-# width_chip = chip_part.shape[1]
-# pixels_here = height_chip * width_chip
-# resize_ratio = np.sqrt(float(pixels_here) / max_pixels_per_chip)
+s = time.time()
+x = xxhash.xxh64()
+x.update(square_img)
+print(x.hexdigest())
+
+print(time.time() - s)
